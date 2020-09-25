4cadfba4bd51af477f8845f6982ebf82b7c97521
João Guilherme Cintra de Freitas Almeida <joaogcfa@al.insper.edu.br>
2020-09-25 15:29:44 -0300

    ciando app.py

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
