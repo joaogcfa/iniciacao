5217225a47c5b8353f167ad79671513efba8e65f
joaogcfa <joaogcfa@al.insper.edu.br>
2020-10-05 14:33:17 -0300

    quebra o dicionario enviado do html no flask

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pyrebase
 
+config = {
+    "apiKey": "AIzaSyDLYrb2MHSNe2kP3Te7RVNnnxne-g2QV20",
+    "authDomain": "iniciacaoeyetracking.firebaseapp.com",
+    "databaseURL": "https://iniciacaoeyetracking.firebaseio.com",
+    "projectId": "iniciacaoeyetracking",
+    "storageBucket": "iniciacaoeyetracking.appspot.com",
+    "messagingSenderId": "508624071073",
+    "appId": "1:508624071073:web:7a8d981b0903297a0b83e0"
 
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+# db.child("usuarios").push({"horario":{"x":"posicao x", "y":"posicao y"}})
 app = Flask(__name__)
 
 
@@ -16,6 +32,19 @@ def hello():
     if request.method == 'POST':
         print('Incoming..')
         print(request.get_json())  # parse as JSON
+        html = request.get_json()
+        for tempo in html:
+            time_stamp_html = tempo
+            posicoes = html[tempo]
+            for itens in posicoes:
+                if itens == "x":
+                    print("x: ", posicoes[itens])
+                else:
+                    print("y: ", posicoes[itens])
+
+
+        #     min_position_html = html[tempo]
+        #     max_position_html = min_position_html
         return 'OK', 200
 
 
